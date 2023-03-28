b862bb57df7099bfb1a5d9f6d90ef9317cba4cfe
par4m <param2k3@gmail.com>
2023-03-28 16:34:04 +0530

    .

---
+++ Web/js/js/dom4.js
@@ -0,0 +1,12 @@
+const link = document.querySelector('a')
+console.log(link.getAttribute('href'))
+
+link.setAttribute('href', 'https://www.thenetninja.co.uk')
+link.innerText = 'The Net Ninja Website'
+
+const mssg = document.querySelector('p')
+
+console.log(mssg.getAttribute('class'))
+mssg.setAttribute('class', 'success')
+console.log(mssg.getAttribute('class'))
+mssg.setAttribute('style', 'color:green')
---
+++ Web/js/js/dom5.js
@@ -0,0 +1,7 @@
+const title = document.querySelector('h1')
+
+/* title.setAttribute('style', 'margin: 50px') */
+
+console.log(title.style.color)
+
+title.style.margin = '50px'
