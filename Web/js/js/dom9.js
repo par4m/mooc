2b5ad7f27720d82b843f9bae537fc961263db401
par4m <param2k3@gmail.com>
2023-03-31 00:40:42 +0530

    .

--- Web/js/js/dom9.js
+++ Web/js/js/dom9.js
@@ -5,19 +5,25 @@ button.addEventListener('click', () => {
   const li = document.createElement('li')
   li.textContent = 'something new to do '
   ul.prepend(li)
-  ul.append(li)
+  /* ul.append(li) */
 })
 
 const items = document.querySelectorAll('li')
+
 items.forEach((item) => {
   item.addEventListener('click', (e) => {
     /* console.log('item is ' + item.innerText) */
+    console.log('event in Li')
     e.target.remove()
   })
 })
 
+ul.onclick = () => {
+  console.log('event in UL')
+}
+
 /* button.addEventListener('click', () => { }) */
 
-button.onclick = () => {
-  alert('loda')
-}
+/* button.onclick = () => { */
+/* alert */
+/* } */
