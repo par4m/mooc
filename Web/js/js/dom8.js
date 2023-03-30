e0fbac98d2eaaa6f62ad0702718cfc3ee9744c52
par4m <param2k3@gmail.com>
2023-03-30 19:15:11 +0530

    .

---
+++ Web/js/js/dom8.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+  console.log('you clicked me')
+})
+
+const items = document.querySelectorAll('li')
+items.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    /* console.log('item is ' + item.innerText) */
+    console.log(e.target)
+    console.log(item)
+  })
+})
