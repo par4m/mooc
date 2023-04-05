436bf8b1a5f208a1369b8c64320b33ef809dd641
par4m <param2k3@gmail.com>
2023-04-06 02:08:04 +0530

    Weather App

--- Web/js/weather/scripts/forecast.js
+++ Web/js/weather/scripts/forecast.js
@@ -1,34 +1,23 @@
 const key = 'SVEZgxiXKMajoGJIuyy2iYH44unVwQsj'
 
+// get weather information
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+  const query = `${id}?apikey=${key}`
+
+  const response = await fetch(base + query)
+  const data = await response.json()
+
+  return data[0]
+}
+
+// get city information
 const getCity = async (city) => {
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
   const query = `?apikey=${key}&q=${city}`
 
   const response = await fetch(base + query)
   const data = await response.json()
+
   return data[0]
 }
-
-// use key from city details for Weather API call
-const getWeather = async (locationKey) => {
-  const base = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}`
-  const query = `?apikey=${key}`
-
-  const response = await fetch(base + query)
-  const data = await response.json()
-  return data[0]
-}
-
-// get City Details to get Key then use that Key for Weather API
-getCity('delhi')
-  .then((data) => {
-    return getWeather(data.Key)
-  })
-  .then((data) => {
-    console.log(data)
-  })
-  .catch((err) => console.log(err))
-
-/* getWeather(202396) */
-/*   .then((data) => console.log(data)) */
-/*   .catch((err) => console.log(err)) */
