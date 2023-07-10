ffa566a9d096ce4152df35c7057a5549f6afbaa1
par4m <param2k3@gmail.com>
2023-07-11 00:37:38 +0530

    jpr

---
+++ c/dsa/linkedlist/ll1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct Node {
+  int data;
+  // struct Node *addr;
+  struct Node *link;
+};
+
+int main(int argc, char *argv[]) {
+
+  struct Node *A;
+  A = NULL; // empty list
+
+  return 0;
+}
