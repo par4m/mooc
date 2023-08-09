0a0fd83021632bd07fc9d663c18b22616fef2479
par4m <param2k3@gmail.com>
2023-08-09 13:41:10 +0530

    packages

--- java/kk/staticExample/Human.java
+++ java/kk/staticExample/Human.java
@@ -7,6 +7,11 @@ public class Human {
     boolean married;
     static long population;
 
+    static void message() {
+        System.out.println("Hello World");
+        // System.out.println(this.age);// cannot use this over here
+    }
+
     public Human(int age, String name, int salary, boolean married) {
         this.age = age;
         this.name = name;
--- java/kk/staticExample/Main.java
+++ java/kk/staticExample/Main.java
@@ -6,9 +6,16 @@ public class Main {
         Human rahul = new Human(20, "Rahul", 150, true);
         System.out.println(param.population);
         System.out.println(Human.population);
+        Main funn = new Main();
+        funn.fun2();
     }
 
     static void fun() {
+        Main obj = new Main();
+        obj.greeting();
+    }
+
+    void fun2() {
         greeting();
     }
 
