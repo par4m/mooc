b9a27198db9b0bb98ba24d477eb191b9ac707cdf
par4m <param2k3@gmail.com>
2023-08-17 01:19:49 +0530

    ds

---
+++ c/strfun/self.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+
+struct code {
+  int i;
+  char c;
+  struct code *ptr;
+};
+
+int main(int argc, char *argv[])
+
+{
+  struct code var1;
+  struct code var2;
+
+  var1.i = 65;
+  var1.c = 'A';
+  var1.ptr = NULL;
+
+  var2.i = 66;
+  var2.c = 'B';
+  var2.ptr = NULL;
+
+  var1.ptr = &var2;
+  printf("%d %c\n", var1.ptr->i, var1.ptr->c);
+  printf("%d %c", var2.i, var2.c);
+  return 0;
+}
